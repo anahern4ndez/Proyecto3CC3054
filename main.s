75b856840cdbe0164c13b66da36b365cfaf22d4a
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-04-21 17:09:27 -0600

    cambie formato de comentarios para poder compilarlo

--- main.s
+++ main.s
@@ -1,5 +1,4 @@
-
-
+﻿
 /*
  Organizacion de Computadoras y Assembler
     Ana Lucia Hernandez 17138
@@ -17,7 +16,7 @@
 
 .data
 .align 2
-//se definen las variables a usar
+@@se definen las variables a usar
 N:
     .word   10
 welcome1:
@@ -29,9 +28,9 @@ ingreso_dato:
 mal:
     .asciz "Ha ingresado mal algun caracter. Solo se puede del abecedario."
 
-ptos1: //puntos del jugador 1
+ptos1: @@puntos del jugador 1
     .word 0
-ptos2:  //puntos del jugador 2
+ptos2:  @@puntos del jugador 2
     .word 0
 welcome2:
     .asciz "******* JUGADOR 2  **************"
@@ -47,7 +46,7 @@ vocal:
     .asciz " "
 
 
-//Empieza el programa
+@@Empieza el programa
 .text
 .align 2
 
@@ -59,10 +58,10 @@ main:
     stmfd sp!,{lr}
     mov r11, #0
 
-    //Links a subrutinas
+    @@Links a subrutinas
     bl ingreso
 
-    //salida a SO
+    @@salida a SO
     mov r0,#0
     mov r3,#0
     ldmfd sp!,{lr}
@@ -75,10 +74,10 @@ ingreso: /* ingreso de nombre */
     ldrne r0,=welcome2
     bl puts
 
-    mov r10, #10 //prueba
+    mov r10, #10 @@prueba
     mov r9, #6
     mul r2,r9, r10
-    ldr r5,=banco //carga la primera posicion del banco de palabras
+    ldr r5,=banco @@carga la primera posicion del banco de palabras
     sub r5, #1
     add r5, r2
 
@@ -96,10 +95,10 @@ ingreso: /* ingreso de nombre */
     ldr r7,=vocal
     ldrb r4, [r7]
     mov r2, r10
-    bl getchar //eliminar del buffer
+    bl getchar @@eliminar del buffer
     bl encontrar_palabra
 
-    mov pc, lr //regreso al main
+    mov pc, lr @@regreso al main
 
 
 encontrar_palabra:
@@ -113,27 +112,27 @@ encontrar_palabra:
     pop {lr}
 
 
-pierde: //el jugador ingreso mal la letra
+pierde: @@el jugador ingreso mal la letra
     cmp r11, #0
     ldreq r1,=ptos1
     ldrne r1,=ptos2
-    moveq r11, #1    //si esta jugando el jugador 1, que cambie
-    movne r11, #0 //si esta jugando el jugador 2, que cambie
+    moveq r11, #1    @@si esta jugando el jugador 1, que cambie
+    movne r11, #0 @@si esta jugando el jugador 2, que cambie
     ldr r0,=error
     bl printf
     pop {lr}
     b ingreso
 
-gana:   //el jugador ingreso bien la palabra
+gana:   @@el jugador ingreso bien la palabra
     mov r0, #0
     cmp r11, #0
     ldreq r0,=ptos1
     ldrne r0,=ptos2
-    moveq r11, #1    //si esta jugando el jugador 1, que cambie
-    movne r11, #0 //si esta jugando el jugador 2, que cambie
+    moveq r11, #1    @@si esta jugando el jugador 1, que cambie
+    movne r11, #0 @@si esta jugando el jugador 2, que cambie
     ldrb r0, [r0]
     add r1, r0, #1
-//    strb r1, [r0]  // esto hay que arreglarlo porque tira un segmentation fault
+@@    strb r1, [r0]  @@ esto hay que arreglarlo porque tira un segmentation fault
     mov r1, r0
     ldr r0,=correcto
     bl printf
